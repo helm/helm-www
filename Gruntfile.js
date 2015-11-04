f06358f8f65757bf3acc1c7442f646e88e26bad5
Jack Francis <jack@deis.com>
2015-11-04 12:55:43 -0800

    chore (mvp) going down the dangerous path of jekyll + grunt
    this is to minify js

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
+      },
+      build: {
+        src: 'javascripts/*.js',
+        dest: 'javascripts/helm.min.js'
+      }
+    }
+  });
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  // Default task(s).
+  grunt.registerTask('default', ['uglify']);
+};
