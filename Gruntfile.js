502576b4ba1734315532232950e2af35293be558
Jack Francis <jack@deis.com>
2015-11-05 14:32:14 -0800

    javascripts (mvp) cleaned up the javascripts

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,7 +7,7 @@ module.exports = function(grunt) {
         banner: '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n'
       },
       build: {
-        src: 'javascripts/*.js',
+        src: ['javascripts/*.js','!javascripts/*.min.js'],
         dest: 'javascripts/helm.min.js'
       }
     }
