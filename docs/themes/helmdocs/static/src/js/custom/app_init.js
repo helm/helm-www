1ef1b8161d2868ec8b10b7e7a4dc35d1baf6b7e5
flynnduism <flynnduism@gmail.com>
2017-07-07 10:25:55 -0700

    Add Permalink #s to Docs header tags (h1 to h6)

--- docs/themes/helmdocs/static/src/js/custom/app_init.js
+++ docs/themes/helmdocs/static/src/js/custom/app_init.js
@@ -74,6 +74,20 @@ menuItems.click(function(f){
   }, 300);
 });
 
+
+// add permalinks to titles
+$(function() {
+  return $("h1, h2, h3, h4, h5, h6").each(function(i, el) {
+    var $el, icon, id;
+    $el = $(el);
+    id = $el.attr('id');
+    icon = '<i class="fa fa-link"></i>';
+    if (id) {
+      return $el.prepend($("<a />").addClass("header-link").attr("href", "#" + id).html(icon));
+    }
+  });
+});
+
 // smooth scroll
 $(function() {
   $('a[href*=#]:not([href=#])').click(function() {
