b9bed012bebfa880023206099cf8f4ae1c226234
flynnduism <flynnduism@gmail.com>
2017-12-06 10:05:00 -0800

    update links for security doc

--- docs/gulpfile.js
+++ docs/gulpfile.js
@@ -255,6 +255,8 @@ gulp.task('clone', function() {
       .pipe(replace('.png)', '.png)'))
       // update tips and tricks link
       .pipe(replace('charts_tips_and_tricks', 'chart-development-tips-and-tricks'))
+      // update security anchor link
+      .pipe(replace('securing_installation', 'securing-your-helm-installation'))
       .pipe(gulp.dest('source/docs/'))
   });
 
