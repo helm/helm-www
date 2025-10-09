9f6213133ef24033d48bc200b1477fbb0d2373f6
Scott Rigby <scott@r6by.com>
2025-10-09 13:22:36 -0400

    Rework CSS and JS and new homepage layout for Docusaurus
    Includes:
    - Sitewide styles using Docusaurus Infima variables
    - Homepage blocks as components
    - Separate styles per component
    - Update home page layout for clearer design
    - Fix custom CSS, including: boat transition, reducing wave wrapper height,
      stacked solution for header content to allow scaling for different viewports
    - Simplify custom JS, favoring docusaurus build-ins
    - Hero block height calculator as client module
    - Add architectural decisions doc to clarify decisions so far and enable
      easier contributions and maintenance
    - Add blog styles and customize docs breadcrumbs
    
    Co-authored-by: Paige Calvert <eura.calvert@gmail.com>
    Signed-off-by: Scott Rigby <scott@r6by.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -3,25 +3,23 @@
 // (when paired with `@ts-check`).
 // There are various equivalent ways to declare your Docusaurus config.
 // See: https://docusaurus.io/docs/api/docusaurus-config
-
-import {themes as prismThemes} from 'prism-react-renderer';
+import { themes as prismThemes } from "prism-react-renderer";
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
-
-const siteURL = process.env.SITE_URL || 'http://localhost:3000';
+const siteURL = process.env.SITE_URL || "http://localhost:3000";
 const rawBaseUrl = process.env.BASE_URL;
 const normalizedBaseUrl =
   rawBaseUrl === undefined
-    ? '/'
-    : rawBaseUrl.endsWith('/')
-      ? rawBaseUrl
-      : rawBaseUrl + '/';
+    ? "/"
+    : rawBaseUrl.endsWith("/")
+    ? rawBaseUrl
+    : rawBaseUrl + "/";
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
-  favicon: 'img/favicon.ico',
+  title: "Helm",
+  tagline: "The package manager for Kubernetes",
+  favicon: "img/favicon.ico",
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
   future: {
@@ -36,48 +34,48 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: "helm", // Usually your GitHub org/user name.
+  projectName: "helm", // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: "throw",
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
   // may want to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: 'en',
-    locales: ['en'],
+    defaultLocale: "en",
+    locales: ["en"],
   },
 
   presets: [
     [
-      'classic',
+      "classic",
       /** @type {import('@docusaurus/preset-classic').Options} */
       ({
         docs: {
-          sidebarPath: './sidebars.js',
+          sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: "https://github.com/helm/helm-www/blob/main/",
         },
         blog: {
           showReadingTime: true,
+          blogSidebarTitle: "All posts",
+          blogSidebarCount: "ALL",
           feedOptions: {
-            type: ['rss', 'atom'],
+            type: ["rss", "atom"],
             xslt: true,
           },
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: "https://github.com/helm/helm-www/blob/main/",
           // Useful options to enforce blogging best practices
-          onInlineTags: 'warn',
-          onInlineAuthors: 'warn',
-          onUntruncatedBlogPosts: 'warn',
+          onInlineTags: "warn",
+          onInlineAuthors: "warn",
+          onUntruncatedBlogPosts: "warn",
         },
         theme: {
-          customCss: './src/css/custom.css',
+          customCss: "./src/css/custom.css",
         },
       }),
     ],
@@ -87,81 +85,147 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
       // Replace with your project's social card
-      image: 'img/docusaurus-social-card.jpg',
+      image: "img/helm-social-card.png",
       colorMode: {
-        respectPrefersColorScheme: true,
+        defaultMode: "light",
+        disableSwitch: true,
       },
       navbar: {
-        title: 'My Site',
+        title: "Helm",
         logo: {
-          alt: 'My Site Logo',
-          src: 'img/logo.svg',
+          alt: "Helm Logo",
+          src: "img/helm.svg",
         },
         items: [
           {
-            type: 'docSidebar',
-            sidebarId: 'tutorialSidebar',
-            position: 'left',
-            label: 'Tutorial',
+            to: "docs",
+            label: "Docs",
+            position: "left",
           },
-          {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
+            href: "https://artifacthub.io/",
+            label: "Charts",
+            position: "left",
+          },
+          { to: "/blog", label: "Blog", position: "left" },
+          {
+            href: "https://github.com/helm/community/",
+            label: "Community",
+            position: "left",
+          },
+          {
+            type: "docsVersionDropdown",
+            position: "right",
+          },
+          {
+            type: "localeDropdown",
+            position: "right",
           },
         ],
       },
       footer: {
-        style: 'dark',
+        style: "dark",
         links: [
           {
-            title: 'Docs',
+            title: "Helm Project",
             items: [
               {
-                label: 'Tutorial',
-                to: '/docs/intro',
+                label: "Source code",
+                href: "https://github.com/helm/helm",
+              },
+              {
+                label: "Blog",
+                to: "blog",
+              },
+              {
+                label: "Events",
+                href: "https://www.cncf.io/community/kubecon-cloudnativecon-events/",
+              },
+              {
+                label: "Code of Conduct",
+                href: "https://github.com/cncf/foundation/blob/master/code-of-conduct.md",
               },
             ],
           },
           {
-            title: 'Community',
+            title: "Charts",
             items: [
               {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+                label: "Introduction",
+                to: "#",
               },
               {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
+                label: "Chart tips & tricks",
+                to: "#",
               },
               {
-                label: 'X',
-                href: 'https://x.com/docusaurus',
+                label: "Developing Charts",
+                to: "#",
+              },
+              {
+                label: "Search 800+ Charts",
+                href: "https://artifacthub.io/",
               },
             ],
           },
           {
-            title: 'More',
+            title: "Development",
             items: [
               {
-                label: 'Blog',
-                to: '/blog',
+                label: "Slack (#helm-dev)",
+                href: "https://kubernetes.slack.com/messages/C51E88VDG",
               },
               {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                label: "Contribution Guide",
+                href: "https://github.com/helm/helm/blob/main/CONTRIBUTING.md",
+              },
+              {
+                label: "Maintainers",
+                href: "https://github.com/helm/helm/blob/main/OWNERS",
+              },
+              {
+                label: "Weekly Meetings",
+                href: "https://github.com/helm/community/blob/main/communication.md#meetings",
+              },
+            ],
+          },
+          {
+            title: "Community",
+            items: [
+              {
+                label: "GitHub Community",
+                href: "https://github.com/helm/community",
+              },
+              {
+                label: "Slack (#helm-users)",
+                href: "https://kubernetes.slack.com/",
+              },
+              {
+                label: "Stack Overflow",
+                href: "https://stackoverflow.com/questions/tagged/kubernetes-helm",
+              },
+              {
+                label: "X",
+                href: "https://x.com/helmpack",
               },
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        logo: {
+          alt: "CNCF Logo",
+          src: "/img/cncf-white.png",
+        },
+        copyright: `<a href="https://www.cncf.io/">We are a Cloud Native Computing Foundation graduated project.</a><br/>© Helm Authors ${new Date().getFullYear()}. Documentation distributed under <a href="https://creativecommons.org/licenses/by/4.0">CC-BY-4.0.</a><br/>© ${new Date().getFullYear()} The Linux Foundation. All rights reserved. The Linux Foundation has registered trademarks and uses trademarks. For a list of trademarks of The Linux Foundation, please see our <a href="https://www.linuxfoundation.org/trademark-usage/">Trademark Usage page</a>.`,
       },
       prism: {
         theme: prismThemes.github,
         darkTheme: prismThemes.dracula,
       },
     }),
+
+  clientModules: [
+    require.resolve("./src/client-modules/heroHeightCalculator.js"),
+  ],
 };
 
 export default config;
