23b54a42069200da749d268fc435920b0dc09020
Paige Calvert <eura.calvert@gmail.com>
2025-10-15 18:24:10 -0600

    Add i18n directory with homepage translations for all locales
    Co-authored by: Scott Rigby <scott@r6by.com>
    Signed-off-by: Paige Calvert <eura.calvert@gmail.com>
    Signed-off-by: Nicolas Fantoni <nfantoni@imolainformatica.it>

--- src/components/HomeGettingStarted/index.js
+++ src/components/HomeGettingStarted/index.js
@@ -6,6 +6,7 @@ import CodeBlock from "@theme/CodeBlock";
 import styles from "./styles.module.css";
 import homeSections from "@site/src/css/home-sections.module.css";
 import homeCards from "@site/src/css/home-cards.module.css";
+import Translate from '@docusaurus/Translate';
 
 export default function HomeGettingStarted() {
   return (
@@ -17,20 +18,31 @@ export default function HomeGettingStarted() {
       )}
     >
       <div className="container">
-        <Heading as="h2">Getting Started</Heading>
+        <Heading as="h2"><Translate id="home.gettingStarted.title" description="Getting started section title">Getting Started</Translate></Heading>
         <div className="row">
           <div className={clsx("col col--6", homeCards.col)}>
             <div className={clsx("card", homeCards.card)}>
               <div className={clsx("card__header", homeCards.card__header)}>
-                <Heading as="h3">Get Helm</Heading>
+                <Heading as="h3"><Translate id="home.gettingStarted.getHelmTitle" description="Get Helm section title">Get Helm</Translate></Heading>
               </div>
               <div className="card__body">
                 <p>
-                  Install Helm with a package manager, or{" "}
-                  <a href="https://github.com/helm/helm/releases/latest">
-                    download a binary
-                  </a>
-                  .
+                  <Translate
+                    id="home.gettingStarted.installHelm"
+                    description="Getting started install with package manager sentence with download link"
+                    values={{
+                      downloadLink: (
+                        <a href="https://github.com/helm/helm/releases/latest">
+                          <Translate
+                            id="home.gettingStarted.installHelm.downloadLink"
+                            description="Download a binary link">
+                            download a binary
+                          </Translate>
+                        </a>
+                      ),
+                    }}>
+                    {'Install Helm with a package manager, or {downloadLink}.'}
+                  </Translate>
                 </p>
 
                 <Tabs>
@@ -53,11 +65,40 @@ export default function HomeGettingStarted() {
                 </Tabs>
 
                 <p>
-                  Once installed, unpack the helm binary and add it to your PATH
-                  and you are good to go! Check the{" "}
-                  <a href="./docs/intro/install/">docs</a> for further{" "}
-                  <a href="./docs/intro/install/">installation</a> and{" "}
-                  <a href="./docs/intro/using_helm/">usage instructions</a>.
+                  <Translate
+                    id="home.gettingStarted.postInstall.instructions"
+                    description="Instructions after installing Helm"
+                    values={{
+                      docsLink: (
+                        <a href="./docs/intro/install/">
+                          <Translate
+                            id="home.gettingStarted.postInstall.docsLink"
+                            description="Link to installation docs">
+                            docs
+                          </Translate>
+                        </a>
+                      ),
+                      installationLink: (
+                        <a href="./docs/intro/install/">
+                          <Translate
+                            id="home.gettingStarted.postInstall.installationLink"
+                            description="Link to installation instructions">
+                            installation
+                          </Translate>
+                        </a>
+                      ),
+                      usageLink: (
+                        <a href="./docs/intro/using_helm/">
+                          <Translate
+                            id="home.gettingStarted.postInstall.usageLink"
+                            description="Link to usage instructions">
+                            usage instructions
+                          </Translate>
+                        </a>
+                      ),
+                    }}>
+                    {'Once installed, unpack the helm binary and add it to your PATH and you are good to go! Check the {docsLink} for further {installationLink} and {usageLink}.'}
+                  </Translate>
                 </p>
               </div>
             </div>
@@ -66,16 +107,35 @@ export default function HomeGettingStarted() {
           <div className={clsx("col col--6", homeCards.col)}>
             <div className={clsx("card", homeCards.card)}>
               <div className={clsx("card__header", homeCards.card__header)}>
-                <Heading as="h3">Get Charts</Heading>
+                <Heading as="h3">
+                  <Translate
+                    id="home.gettingStarted.getChartsTitle"
+                    description="Getting started get charts title">
+                    Get Charts
+                  </Translate>
+                </Heading>
               </div>
               <div className="card__body">
                 <p>
-                  Visit <a href="https://artifacthub.io">Artifact Hub</a> to
-                  explore{" "}
-                  <a href="https://artifacthub.io/packages/search?kind=0">
-                    Helm charts
-                  </a>{" "}
-                  from numerous public repositories.
+                  <Translate
+                    id="home.gettingStarted.artifactHub"
+                    description="Artifact Hub link"
+                    values={{
+                      artifactHubLink: (
+                        <a href="https://artifacthub.io">Artifact Hub</a>
+                      ),
+                      helmChartsLink: (
+                        <a href="https://artifacthub.io/packages/search?kind=0">
+                          <Translate
+                            id="home.gettingStarted.artifactHub.helmChartsLink"
+                            description="Helm charts link">
+                            Helm charts
+                          </Translate>
+                        </a>
+                      ),
+                    }}>
+                    {'Visit {artifactHubLink} to explore {helmChartsLink} from numerous public repositories.'}
+                  </Translate>
                 </p>
 
                 <a href="https://artifacthub.io/">
