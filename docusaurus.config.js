71aad3d7ec1b0c288eac6646d07b03637df05fef
Nicolas Fantoni <nfantoni@imolainformatica.it>
2025-10-29 19:52:38 +0100

    start it lenguage
    Signed-off-by: Nicolas Fantoni <nfantoni@imolainformatica.it>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -49,7 +49,7 @@ const config = {
 
   i18n: {
     defaultLocale: "en",
-    locales: ["en", "de", "el", "es", "fr", "ja", "ko", "pt", "ru", "uk", "zh"],
+    locales: ["en", "de", "el", "es", "fr", "it", "ja", "ko", "pt", "ru", "uk", "zh"],
     localeConfigs: {
       en: {
         htmlLang: "en-us",
@@ -63,6 +63,9 @@ const config = {
       },
       fr: {
         label: "Français (French)",
+      },
+      it: {
+        label: "Italiano (Italian)",
       },
       el: {
         label: "Ελληνικά (Greek)",
