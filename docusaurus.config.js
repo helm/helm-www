088178b1e0e27e35c9d00d0295707b1b34c3277e
Terry Howe <terrylhowe@gmail.com>
2025-11-24 12:53:01 -0700

    chore: update-docs.sh script run on v3
    Signed-off-by: Terry Howe <terrylhowe@gmail.com>

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -130,7 +130,7 @@ const config = {
             // To-do: add this snippet to automation for bumping the version for each new release:
             // `for f in i18n/*/docusaurus-plugin-content-docs/current.json; do jq '."version.label".message = "4.0.0"' "$f" > "$f.tmp" && mv "$f.tmp" "$f"; done`
             current: { label: "4.0.0" },
-            3: { label: "3.19.0", path: "v3" },
+            3: { label: "3.19.2", path: "v3" },
             2: { label: "2.17.0", path: "v2" },
           },
         },
