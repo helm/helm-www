48ad276db59a07cc5d4f3a21857bf466c7a111ef
Scott Rigby <scott@r6by.com>
2025-12-05 15:33:48 -0500

    Make docs tags more accessible
    We aren't currently using docs tags, but this could be a good way to categorize
    docs content in addition to the sidebar.
    
    This PR swizzles two components:
    - the Docs/Footer, in order to remove the inline tags
    - the Docs/Layout, in order to add the inline tags below the version badge
    
    Orthogonal to https://github.com/helm/helm-www/issues/1864, but could enhance
    that effort by providing links to pages that group certain types of docs
    content together in various ways.
    
    Signed-off-by: Scott Rigby <scott@r6by.com>

--- src/theme/DocItem/Footer/index.js
+++ src/theme/DocItem/Footer/index.js
@@ -1,37 +1,27 @@
-import React from 'react';
-import clsx from 'clsx';
-import {ThemeClassNames} from '@docusaurus/theme-common';
-import {useDoc} from '@docusaurus/plugin-content-docs/client';
-import TagsListInline from '@theme/TagsListInline';
-import EditMetaRow from '@theme/EditMetaRow';
+import React from "react";
+import clsx from "clsx";
+import { ThemeClassNames } from "@docusaurus/theme-common";
+import { useDoc } from "@docusaurus/plugin-content-docs/client";
+
+import EditMetaRow from "@theme/EditMetaRow";
 export default function DocItemFooter() {
-  const {metadata} = useDoc();
-  const {editUrl, lastUpdatedAt, lastUpdatedBy, tags} = metadata;
-  const canDisplayTagsRow = tags.length > 0;
+  const { metadata } = useDoc();
+  const { editUrl, lastUpdatedAt, lastUpdatedBy } = metadata;
+
   const canDisplayEditMetaRow = !!(editUrl || lastUpdatedAt || lastUpdatedBy);
-  const canDisplayFooter = canDisplayTagsRow || canDisplayEditMetaRow;
+  const canDisplayFooter = canDisplayEditMetaRow;
   if (!canDisplayFooter) {
     return null;
   }
   return (
     <footer
-      className={clsx(ThemeClassNames.docs.docFooter, 'docusaurus-mt-lg')}>
-      {canDisplayTagsRow && (
-        <div
-          className={clsx(
-            'row margin-top--sm',
-            ThemeClassNames.docs.docFooterTagsRow,
-          )}>
-          <div className="col">
-            <TagsListInline tags={tags} />
-          </div>
-        </div>
-      )}
+      className={clsx(ThemeClassNames.docs.docFooter, "docusaurus-mt-lg")}
+    >
       {canDisplayEditMetaRow && (
         <EditMetaRow
           className={clsx(
-            'margin-top--sm',
-            ThemeClassNames.docs.docFooterEditMetaRow,
+            "margin-top--sm",
+            ThemeClassNames.docs.docFooterEditMetaRow
           )}
           editUrl={editUrl}
           lastUpdatedAt={lastUpdatedAt}
